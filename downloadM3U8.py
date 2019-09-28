6f5b19e04cc647d6dc695c74297845bd34da1c02
Pei <jp96201.pj@gmail.com>
2019-09-28 15:38:23 +1200

    Update downloadM3U8.py

--- downloadM3U8.py
+++ downloadM3U8.py
@@ -1,20 +1,31 @@
 # command line to download m3u8 video file
 import os
-file = open("m3u8url") # read m3u8 link list from this file
+file = open("m3u8url")
 X = file.readlines()
 file.close()
 
 
 def download(url, n):
-    cmd = "ffmpeg -i "+url.strip("\n")+" /xxx/video/v"+str(n)+".mp4"
-    print(cmd)
-    os.system(cmd)
-    print("download done.")
+    folder = '/xxx/video/'
+    file = folder+'v'+str(n)+".mp4"
+    new_file = folder+'v'+str(n+1)+".mp4"
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    if not os.path.exists(file):
+        cmd = "ffmpeg -i "+url.strip("\n")+" "+file
+        print(cmd)
+        os.system(cmd)
+    else:
+        cmd = "ffmpeg -i " + url.strip("\n") + " " + new_file
+        print(cmd)
+        os.system(cmd)
 
 
 n = 0
 for url in X:
     download(url, n)
     n = n + 1
+
+print("download done.")
 
 
